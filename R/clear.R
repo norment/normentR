d175d96dc240b75e995e4e378e21ea8fb4b6191b
Daniel Roelfs <daniel@roelfs.net>
2019-04-30 20:38:49 +0200

    added clear function

---
+++ R/clear.R
@@ -0,0 +1,58 @@
+#' Clear variables
+#'
+#' This function is a wrapper for the `rm()` function. This function takes a 
+#' pattern, which can include a wildcard (*, asterisk) in either the argument 
+#' `remove` or `keep` to either remove or keep all objects matching a string.
+#' It's possible to input multiple variable names in the `remove` argument, but 
+#' it's not (yet) possible for the `keep` argument.
+#'
+#' @usage
+#' clear()
+#' clear(remove = "var")
+#' clear(remove = "var*")
+#' clear(keep = "var")
+#' clear(keep = "var*")
+
+#' @export
+
+clear <- function(remove = NULL, keep = NULL) {
+  
+  pos = 1
+  
+  if (is.null(remove) && is.null(keep)) {
+    
+    rm(list = ls(pos = pos), pos = pos)
+    
+  } else if (is.character(remove) || is.character(keep)) {
+    
+    if (!is.null(remove) && is.null(keep)) {
+      
+      for (i in remove) {
+        rm(list = ls(pattern = glob2rx(i), pos = pos), pos = pos)
+      }
+      
+    } else if (is.null(remove) && !is.null(keep)) {
+      
+      has.wc[it] <- grep(i, "*")
+      
+      if (!is.character(keep)) {
+        warning("Input is not a character")
+      } else if (length(has.wc) == 0) {
+        rm(list = setdiff(ls(pos = pos), keep), pos = pos)
+      } else {
+        rm(list = setdiff(ls(pos = pos), ls(pattern = glob2rx(keep), pos = pos)), pos = pos)
+      }
+      
+    } else {
+      
+      warning("Multiple arguments used, please use only one")
+      
+    }
+    
+  } else {
+    
+    warning("\nInput must be strings!")
+    
+  }
+  
+}
