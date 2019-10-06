9505e274dbe7d8a3c0bc3f610af9d7f1e93e6f87
Daniel Roelfs <daniel@roelfs.net>
2019-10-06 08:50:52 +0200

    add cohens_d() function

---
+++ R/cohens_d.R
@@ -0,0 +1,34 @@
+#' Calculate Cohen's d
+#'
+#' Calculate Cohen's d from the input to a T-test. Assign the input vectors you
+#' would input into a `t.test()` function into this function. This function will
+#' in turn calculate Cohen's D.
+#' 
+#' @param n Number of calculations
+#' @param alpha Alpha level (default at 0.05)
+#'
+#' @usage 
+#' CohensD <- cohens_d(x = data1$x, y = data2$x)
+#' CohensD <- cohens_d(x = rnorm(n = 20, mean = 5, sd = 1),
+#'                     y = rnorm(n = 20, mean = 6, sd = 0.5))
+#'
+#' @export
+
+cohens_d <- function(x = NULL, y = NULL) {
+  
+  if (is.null(x)) { warning("x cannot be empty, no default is available for x, please assign a numeric vector"); return() }
+  if (is.null(y)) { warning("y cannot be empty, no default is available for y, please assign a numeric vector"); return() }
+  
+  if (is.character(x)) { warning("x cannot be a character string, please assign a numeric vector"); return() }
+  if (is.character(y)) { warning("x cannot be a character string, please assign a numeric vector"); return() }
+  
+  M1 <- mean(x)
+  s1 <- sd(x)
+  M2 <- mean(y)
+  s2 <- sd(y)
+  
+  sdp <- sqrt((s1^2 + s2^2) / 2)
+  d <- (M1 - M2) / sdp
+  
+  return(d)
+}
